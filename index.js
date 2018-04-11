d882e3bf3f69050eb6e0d042940b93fcee92b399
Peter Yu <PeterYu@gmail.com>
2018-04-11 12:11:37 -0400

    added server.js file

---
+++ index.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const express = require('express');
+const {PORT} = require('./config');
+const app = express();
+
+app.use( '/', express.static(__dirname + '/public') );
+app.use( '/node_modules', express.static(__dirname + '/node_modules') );
+app.use( '/src', express.static(__dirname + '/src') );
+
+app.listen(PORT, function() {
+  console.log(`The server at port ${PORT} is listening.`);
+});
